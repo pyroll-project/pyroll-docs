2381cd0635f2537ecca4b472fc6abd86bcddfa07
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-07-25 16:36:06 +0200

    incr version

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = '2022, The PyRolL Project'
 author = 'Max Weiner, Christoph Renzing, Matthias Schmidtchen'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.0.3'
 
 # -- General configuration ---------------------------------------------------
 
